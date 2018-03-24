6a2d84a4b2f0c3f86315575424fdcce9585b2088
The-r3al-sanjay <35691890+The-r3al-sanjay@users.noreply.github.com>
2018-03-24 19:15:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,19 @@
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix='l.')
+
+
+@bot.event
+async def on_ready():
+        """Shows bot's status"""
+        print("----------------")
+        print("Logged in as:")
+        print("Name : {}".format(bot.user.name))
+        print("ID : {}".format(bot.user.id))
+        print("Py Lib Version: %s"%discord.__version__)
+        print("----------------")
+
 if not os.environ.get('TOKEN'):
     print("no token found!")
 bot.run(os.environ.get('TOKEN').strip('"'))
